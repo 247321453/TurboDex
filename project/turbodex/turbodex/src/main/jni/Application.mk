2b2bac8f222defd739418d35e74c5e902c323377
keyongyu <247321453@qq.com>
2016-07-14 12:09:13 +0800

    io

--- project/turbodex/turbodex/src/main/jni/Application.mk
+++ project/turbodex/turbodex/src/main/jni/Application.mk
@@ -1,4 +1,3 @@
-NDK_TOOLCHAIN_VERSION=4.8
 APP_ABI := arm64-v8a armeabi armeabi-v7a x86 x86_64
 APP_STL=gnustl_static
 APP_CPPFLAGS := -std=c++11 -fexceptions -frtti
--- project/turbodex/turbodex/src/main/jni/core/FastLoadDex.cpp
+++ project/turbodex/turbodex/src/main/jni/core/FastLoadDex.cpp
@@ -1,6 +1,6 @@
 #include "FastLoadDex.h"
 #include "HookCore.h"
-
+#include<fcntl.h>
 #define DEX2OAT_BIN "/system/bin/dex2oat"
 
 #define DEBUG 1
@@ -9,6 +9,7 @@ static bool hooked = false;
 static bool enable = false;
 
 int (*org_execv)(const char *name, char **argv);
+int (*org_open)(const char* path, int mode);
 
 int my_execv(const char *name, char **argv) {
 		#ifdef DEBUG
@@ -22,14 +23,41 @@ int my_execv(const char *name, char **argv) {
   		return org_execv(name, argv);
 }
 
+int my_open(const char* path, int mode){
+	if(enable && strcmp(path, "/data/data/com.wadahana.testhook/a.txt") == 0) {
+		path = "/data/data/com.wadahana.testhook/b.txt";
+	}
+		
+  	return org_open(path, mode);
+}
+
 void enableFastLoadDex() {
 	if(!hooked) {
 		HOOK(execv);
+		HOOK(open);
 		hooked = true;
 	}
 	enable = true;
 }
 
 void closeFastLoadDex() {
+	char temp[]="hello,abc!";
+
+	int fd = 0;
+
+	char pathname[255];
+
+	if((fd=open("/data/data/com.wadahana.testhook/a.txt",O_WRONLY|O_CREAT,0640))==-1)
+
+	{
+		
+	}else{
+
+		int len=strlen(temp)+1;
+
+		write(fd,temp,len);
+
+		close(fd);
+	}
 	enable = false;
 }
