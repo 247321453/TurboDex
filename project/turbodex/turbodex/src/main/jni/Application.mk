ff49b9748b0452a6833c2cfbad468a787487ecfa
keyongyu <247321453@qq.com>
2016-07-19 13:24:30 +0800

    arm64 x86_64

--- project/turbodex/turbodex/src/main/jni/Application.mk
+++ project/turbodex/turbodex/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 APP_PLATFORM :=android-10
-APP_ABI := arm64-v8a armeabi armeabi-v7a x86 x86_64
+APP_ABI := armeabi x86
 APP_STL=gnustl_static
 APP_CPPFLAGS := -std=c++11 -fexceptions -frtti
 APP_CPPFLAGS +=-fpermissive
